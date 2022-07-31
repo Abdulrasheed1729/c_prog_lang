171bf0d60d145ce8be103e06a88325463c9ca5d2
Abdulrasheed Fawole <fawomath@gmail.com>
2022-07-31 11:24:10 +0100

    initial commit

--- hello.c
+++ hello.c
@@ -1,15 +1,6 @@
-#include <stddef.h>
 #include <stdio.h>
 
 int main() {
-
-  printf("Hello World I\'m now writing in CCCCCCCCC.\n");
-
-  if (2 < 1) {
-    printf("Hello World");
-  } else {
-    printf("Brr...");
-  }
-
+  printf("Hello World\n");
   return 0;
 }
